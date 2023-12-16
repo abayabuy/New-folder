34e7d320820be5f681de5192a9230df42ae0fcb2
abayabuy <anqaabaygum@gmail.com>
2023-12-16 15:14:01 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,85 @@
+// let nama = 'muhamad akbar syuhada'
+// let usia = 17
+// let tinggiBadan = 165.5
+// let beratBadan
+// let pacar = 3
+
+// beratBadan = 80
+
+// if(pacar == null) {
+//     pacar = 'belum punya'
+// }else {
+//     pacar = 'sudah punya' 
+// }
+
+// let saldoAwal = 100000
+// let saldoTambahan = 20000
+// const hutang = 50000
+// const saldoAkhir = saldoAwal + saldoTambahan - hutang
+
+// // alert(`nama saya ${nama} umur saya ${usia} tinggi badan saya ${tinggiBadan}cm berat badan saya ${beratBadan}kg pacar saya ${pacar}`)
+// alert(`saldo awal saya Rp${saldoAwal} saldo tambahan saya Rp${saldoTambahan} jadi total saldo saya saat ini adalah Rp${saldoAkhir}`)
+
+
+// let namaSiswa = ['abay', 'ikbar', 'akbar']
+// namaSiswa.push('kafka', 'rezi', 'rasya')
+// namaSiswa.shift()
+// namaSiswa.pop()
+// alert(namaSiswa)
+
+
+// const namaSiswa = [`abay`, `ikbar`, `akbar`]
+// for (let i = 0; i < namaSiswa.length; i++) {
+//     console.log(namaSiswa[i])
+// }
+
+//tugas kecil
+
+//1
+// Meminta input
+// let saldoAwal = prompt('berapa saldo awal kamu')
+// let saldoTambahan = prompt('berapa saldo yang ingin di tambah kan')
+// let hutang = prompt('berapa hutangmu')
+
+// Rumus menghitung saldo akhir
+// const saldoAkhir = saldoAwal + saldoTambahan - hutang 
+
+// Menampilkan hasil saldo akhir
+// console.log('saldo akhir anda adalah: ' + saldoAkhir)
+
+// // Fungsi untuk menghitung saldo akhir
+// function hitungSaldoAkhir(saldoAwal, pemasukan, pengeluaran) {
+//     return saldoAwal + pemasukan - pengeluaran;
+//   }
+  
+//   // Meminta input dari pengguna
+//   var saldoAwal = parseFloat(prompt("Masukkan saldo awal:"));
+//   var pemasukan = parseFloat(prompt("Masukkan jumlah pemasukan:"));
+//   var pengeluaran = parseFloat(prompt("Masukkan jumlah pengeluaran:"));
+  
+//   // Memanggil fungsi hitungSaldoAkhir
+//   var saldoAkhir = hitungSaldoAkhir(saldoAwal, pemasukan, pengeluaran);
+  
+//   // Menampilkan saldo akhir
+//   console.log("Saldo akhir Anda adalah: " + saldoAkhir);
+
+// let hari = new Date().getDay() 
+// console.log(`hari ini adalah hari ${hari}`)
+
+// // Mendapatkan tanggal saat ini
+// let tanggalSekarang = new Date();
+
+// // Array untuk menyimpan nama-nama hari
+// let namaHari = ["Minggu", "Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu"];
+
+// // Mendapatkan indeks hari dari tanggal saat ini
+// let indeksHari = tanggalSekarang.getDay();
+
+// // Mendapatkan nama hari berdasarkan indeks
+// let hariSekarang = namaHari[indeksHari];
+
+// // Menampilkan hasil
+// console.log(`hari ini adalah hari ${hariSekarang}`);
+
+
+  
